a17a4186ec4a95e6d6b5aa470b96863613eed166
MattB859 <mateus756@hotmail.com>
2021-11-16 12:54:27 +0000

    Add todo list app

---
+++ todo/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+
+
+class TestDjango(TestCase):
+
+    def testing(self):
+        self.assertEqual(1, 1)
+
+    def testing2(self):
+        self.assertEqual(1, 1)
+
+    def testing3(self):
+        self.assertEqual(1, 1)
+
+    def testing4(self):
+        self.assertEqual(1, 1)
